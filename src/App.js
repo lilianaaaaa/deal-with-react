c81b63ed4e01d82812ae05b86e6b121f26af1048
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 14:47:15 +0900

    URL parameter, query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
   return (
@@ -12,11 +13,18 @@ const App = () => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/profile/liliana">liliana profile</Link>
+        </li>
+        <li>
+          <Link to="/profile/benny">benny profile</Link>
+        </li>
       </ul>
       <hr/>
       {/* 현재 경로에 따라 다른 컴포넌트 보여주기 */}
       <Route path="/" component={Home} exact={true}/> 
-      <Route path="/about" component={About}/>
+      <Route path={['/about', '/info']} component={About}/>
+      <Route path="/profile/:username" component={Profile}/>
     </div>
   );
 };
