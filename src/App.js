013000562079e0d03a77140c3bbe0efb4bad4192
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 15:44:12 +0900

    리액트 라우터 부가기능 - Switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 //import Profile from './Profile';
@@ -24,10 +24,19 @@ const App = () => {
       </ul>
       <hr/>
       {/* 현재 경로에 따라 다른 컴포넌트 보여주기 */}
+      <Switch>
       <Route path="/" component={Home} exact={true}/> 
       <Route path={['/about', '/info']} component={About}/>
       <Route path="/profiles" component={Profiles}/>
       <Route path="/history" component={HistorySample}/>
+      <Route render={({location}) => (
+        <div>
+          <h2>This page does not exist</h2>
+          <p>{location.pathname}</p>
+        </div>
+      )}
+      />
+      </Switch>
     </div>
   );
 };
