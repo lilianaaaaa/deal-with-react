97e2e2371ac37534163e89c458411bb53b3a426d
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 16:54:43 +0900

    뉴스 뷰어 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import {useState} from 'react';
-import axios from 'axios';
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async() => {
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=01e8ff68ce2349adaf8e4257d847807e',
-      );
-      setData(response.data)
-    }catch(e){
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>getData</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-    </div>
-  );
+  return <NewsList/>;
 };
 
 export default App;
