5879b56b289a53afb3ae99040abd9b02623f088b
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 16:51:43 +0900

    news API url로 변경

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-      setData(response.data);
-    })
-  }
+  const onClick = async() => {
+    try{
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=01e8ff68ce2349adaf8e4257d847807e',
+      );
+      setData(response.data)
+    }catch(e){
+      console.log(e);
+    }
+  };
   return (
     <div>
       <div>
