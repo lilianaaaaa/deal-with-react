6c792781174d3ff441f02b9a4e00d826a32d2300
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 14:12:02 +0900

    Route 컴포넌트로 특정 주소에 컴포넌트 연결, Link 컴포넌트 사용

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>react router basic study project</p>
+        </div>
+    );
+};
+
+export default About;
