d05ca76b1f5682e780089ab7dfa7cd570150d437
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 11:55:40 +0900

    항목추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
@@ -21,9 +21,27 @@ const App = () => {
       checked: true,
     }
   ]); //todos array -> TodoList props
+
+  //고유값으로 사용될 id
+  //ref 사용하여 변수 담기: id값은 렌더링되는 정보가 아니기 때문
+  const nextId = useRef(4);
+
+  //컴포넌트의 성능 아낄수 있도록 useCallback으로 감싼다.
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todo.concat(todo));
+      nextId.current += 1; //nextId 1씩 더하기
+    },
+    [todos],
+  );
   return( 
   <TodoTemplate>
-    <TodoInsert/>
+    <TodoInsert onInsert={onInsert}/>
     <TodoList todos={todos}/>
   </TodoTemplate>
   );
