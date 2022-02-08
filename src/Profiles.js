c0859966f35668ad3697d9b4b5276aae007ee995
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-08 15:47:18 +0900

    리액트 라우터 부가기능 - NavLink

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,16 +1,20 @@
-import {Link, Route} from 'react-router-dom';
+import {NavLink, Route} from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+    const activeStyle = {
+        background: 'black',
+        color: 'white'
+    };
     return(
         <div>
             <h3>User List:</h3>
             <ul>
                 <li>
-                <Link to="/profiles/liliana">liliana</Link>
+                <NavLink activeStyle={activeStyle} to="/profiles/liliana">liliana</NavLink>
                 </li>
                 <li>
-                <Link to="/profiles/benny">benny</Link>
+                <NavLink activeStyle={activeStyle} to="/profiles/benny">benny</NavLink>
                 </li>
             </ul>
       <Route path="/profiles" exact render={() => <div>Please select User.</div>} />{/* 컴포넌트에 props 별도로 넣어주고 싶을때 사용 exact={true}와 같은 의미 */}
