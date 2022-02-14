eb6e31df765d0ae8abb5f9aa692ef4c54c3b1a3c
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-14 21:50:57 +0900

    react-virtualized 사용한 렌더링

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,38 @@
+import React, {useCallback} from 'react';
+import {List} from 'react-virtualized';
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+
+    //항목 렌더링할때 쓰는 함수
+    const rowRenderer = useCallback(
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return(
+                <TodoListItem
+                    todo={todo}
+                    key={key}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                    style={style}
+                />
+            );
+        },
+        [onRemove, onToggle, todos],
+    )
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-            ))}
-        </div>
+        <List 
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline:'none'}}
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
