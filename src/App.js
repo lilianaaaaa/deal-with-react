fbe7be17a1a4068fd3dd27f96b26d1fd63dbe000
lilianaaaaa <lilianaaa804@gmail.com>
2022-02-15 13:36:03 +0900

    Context API 사용법 익히기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App = () => {
   return (
-    <div>
-      <ColorBox/>
-    </div>
+    /* Provider 사용: value값 반드시 명시해주어야 함. */
+    <ColorContext.Provider value={{color: 'red'}}>
+      <div>
+        <ColorBox/>
+      </div>
+    </ColorContext.Provider>
   );
 };
 
