179c195d6e34458c67467917609ba6f7829da2a9
lilianaaaaa <lilianaaa804@gmail.com>
2022-03-17 18:28:44 +0900

    Hooks 사용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,46 +1,17 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from "../components/Counter";
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
+
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease}/>
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease}/>
     );
-};
+}
 
-// const mapStateToProps = state => ({
-//     number: state.counter.number,
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//     increase: () => {
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         dispatch(decrease());
-//     },
-// });
-
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps,
-// )(CounterContainer);
-
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    dispatch => 
-        bindActionCreators(
-        {
-            increase,
-            decrease
-        },
-        dispatch,
-        ),
-    // dispatch => ({
-    //     increase: () => dispatch(increase()),
-    //     decrease: () => dispatch(decrease()),
-    // }),
-)(CounterContainer);
+export default CounterContainer;
