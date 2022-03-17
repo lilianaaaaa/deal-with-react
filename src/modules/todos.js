d36cc24a387af7731cfb61050b5ca89d099390a9
lilianaaaaa <lilianaaa804@gmail.com>
2022-03-17 17:54:31 +0900

    immer 사용

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { createAction, handleActions } from "redux-actions";
 
 //액션타입 정의
@@ -89,23 +90,26 @@ const initialState = {
 
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, {payload: input}) => ({ ...state, input}),
-        [INSERT]: (state, {payload: todo}) => ({
-            ...state,
-            todos: state.todos.concat(todo),
+        [CHANGE_INPUT]: (state, {payload: input}) =>
+        produce(state, draft => {
+            draft.input = input;
         }),
-        [TOGGLE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.map(todo => 
-                todo.id === id ? {...todo, done: !todo.done} : todo,
-            ),
+        [INSERT]: (state, {payload: todo}) => 
+        produce(state, draft => {
+            draft.todos.push(todo);
         }),
-        [REMOVE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.filter(todo => todo.id !== id),
+        [TOGGLE]: (state, {payload: id}) =>
+        produce(state, draft => {
+            const todo = draft.todos.find(todo => todo.id === id);
+            todo.done = !todo.done;
+        }),
+        [REMOVE]: (state, {payload: id}) => 
+        produce(state, draft => {
+            const index = draft.todos.findIndex(todo => todo.id === id);
+            draft.todos.splice(index, 1);
         }),
     },
-    initialState
+    initialState,
 );
 
 export default todos;
