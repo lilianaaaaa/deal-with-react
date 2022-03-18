080d7de388eb66c604112943c0bf788883f4abfc
lilianaaaaa <lilianaaa804@gmail.com>
2022-03-18 15:01:56 +0900

    redux-thunk 적용

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
+//import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
